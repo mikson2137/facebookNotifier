2cadfb2237143f094eb26968696bedd062562dd1
miksonek1 <mikolaj.sitek1@gmail.com>
2022-03-15 00:39:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import time
+
+import discord
+import undetected_chromedriver as uc
+from discord import Webhook, RequestsWebhookAdapter
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def webhook_sender(uzyt, grupa, desc, when, jakagrupa):
+    embed = discord.Embed(color=0xff0000)
+    embed = discord.Embed(title="Znaleziono nowy post", url=jakagrupa)
+    embed.add_field(name="Nazwa użytkownika", value=uzyt, inline=True)
+    embed.add_field(name="Grupa", value=grupa, inline=True)
+    embed.add_field(name="Czas", value=when, inline=True)
+    embed.add_field(name="Opis", value=desc, inline=False)
+
+    webhook = Webhook.from_url(
+        "DISCORD_WEBHOOK",
+        adapter=RequestsWebhookAdapter())
+
+    webhook.send(content="@everyone", wait=False, username="Facebook Notificator",
+                 avatar_url="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1024px-Facebook_f_logo_%282019%29.svg.png",
+                 tts=False, file=None, files=None, embed=embed, allowed_mentions=None)
+
+
+def login():
+    driver.get("https://www.facebook.com/login")
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+        (By.XPATH, "//button[@title='Zezwól na korzystanie z niezbędnych i opcjonalnych plików cookie']"))).click()
+    time.sleep(0.5)
+    email = driver.find_element_by_xpath("//input[@placeholder='Adres e-mail lub numer telefonu']").send_keys(
+        "EMAIL")
+    time.sleep(1)
+    passw = driver.find_element_by_xpath("//input[@placeholder='Hasło']").send_keys("PASSWORD")
+    time.sleep(0.5)
+    complete = driver.find_element(by=By.XPATH, value="//button[@name='login']").click()
+    time.sleep(1)
+
+
+def group():
+    time.sleep(2)
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located(
+        (By.XPATH, "//body//div//article[1]")))
+    newest = driver.find_element_by_xpath("//body//div//article[1]").text
+    newest = newest.splitlines()
+    uzyt_grupa = newest[0].split(">")
+    desc = newest[1]
+    when = newest[2]
+    uzyt = uzyt_grupa[0]
+    grupa = uzyt_grupa[1]
+    return uzyt, grupa, desc, when
+
+
+if __name__ == '__main__':
+    linki = {
+        '1': "GROUP_LINK",
+        '2': "GROUP_LINK",
+        '3': "GROUP_LINK",
+        '4': "GROUP_LINK",
+        '5': "GROUP_LINK"
+    }
+    driver = uc.Chrome()
+    driver.maximize_window()
+    login()
+    driver.execute_script(f"window.open('https://www.google.com', 'new_window')")
+    input()
+    for i in range(5):
+        i = i + 1
+        link = str(i)
+        driver.execute_script(f"window.open('{linki[link]}', '{i}')")
+        print(i)
+        time.sleep(0.5)
+
+    time.sleep(0.5)
+    olduser = []
+
+    while True:
+        for i in range(5):
+            i = i + 1
+            driver.switch_to.window(driver.window_handles[i])
+            driver.refresh()
+            time.sleep(1)
+            danegrupy = group()
+            link = str(i)
+            if danegrupy[2] not in olduser:
+                webhook_sender(danegrupy[0], danegrupy[1], danegrupy[2], danegrupy[3], linki[link])
+                olduser.append(danegrupy[2])
+                print(olduser)
+
+        time.sleep(60)
